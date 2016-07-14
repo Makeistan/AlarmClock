023105529ae4c4624d335482d0fb5648e9c9a2f3
Quswar Mahmood Abid <bsee14013@itu.edu.pk>
2016-07-14 16:17:14 +0500

    Updated Discription

--- AlarmClock.ino
+++ AlarmClock.ino
@@ -2,9 +2,11 @@
 This code is published in public domain.
 Any part of it or whole code can be edited/removed/modified, but reproducing and publishing it can be done only by providing proper credit to the resource (make-i-stan).
 
-The Sketch is a simple 7-Segment-LED driver using Arduino UNO with direct addressing all LED segments. Each LED display has an enable terminal so each time, on;y one LED dispalys and a number is shown at one display only.
-Since it is targeted for begineers, direct addressing is avoided, and multiplexing logic is emplyed.
+The Sketch is a simple 7-Segment-LED driver using Arduino UNO with direct addressing all LED segments of all displays at the same time (see Fritzing for detail).
+Each LED display has an enable terminal, so each time only one LED dispalys and a number is shown at one display only, keeping others off.
+Since it is targeted for beginners, direct addressing is avoided at each terminal like the previous version, and multiplexing logic is employed.
 Additional feature of this code is to make use of displaying digits on 7-Segment-LED display. So we will add a buzzer and timing mechanism to make Alarm Clock out of it.
+In this way, a number displaying circuit can be turned into clock and an alarm eventually.
 
 Specially prepared for Arduino Nights at Make-i-stan (Pakistan's first and only Makerspace).
 For more details, visit www.Make-i-stan.co or www.Facebook.com/Makeistan or www.Twitter.com/Makeistan
